e84a86d8e65105a672eff99bbbddac14118c0add
Lei Wang <LeiW1024@users.noreply.github.com>
2026-03-18 18:42:46 +0100

    Auth feature: authentication & session management

---
+++ src/app/auth/error/page.tsx
@@ -0,0 +1,11 @@
+// app/auth/error/page.tsx
+import { Suspense } from "react";
+import AuthErrorContent from "./AuthErrorContent";
+
+export default function AuthErrorPage() {
+  return (
+    <Suspense>
+      <AuthErrorContent />
+    </Suspense>
+  );
+}
---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,6 @@
+// app/auth/login/page.tsx
+import AuthPageWrapper from "@/components/forms/AuthPageWrapper";
+
+export default function LoginPage() {
+  return <AuthPageWrapper page="login" />;
+}
